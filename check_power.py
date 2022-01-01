33cb1c9559f7b7ce44cde35c113b472d79d6193c
Rajdeep Chakraborty <96816530+raj-ch017@users.noreply.github.com>
2022-01-01 21:04:51 +0530

    Changed the name of the function to match the script name
    The name of the function was changed from "is_power_of()" to "check_power()" so as to match the name of the script.

--- check_power.py
+++ check_power.py
@@ -1,11 +1,11 @@
-def is_power_of(number,base):
+def check_power(number,base):
     if number < base and number != 1:
         return bool(0)  #converts value to boolean type
     num1 = number / base
     if num1 == 1 or number == 1:
         return bool(1)
-    return is_power_of(num1,base)
+    return check_power(num1,base)
 
-print(is_power_of(10,2))
-print(is_power_of(8,2))
+print(check_power(10,2))
+print(check_power(8,2))
     
