1c27d84b68e9325a71be5bf25c9aca14fa053b58
Rajdeep Chakraborty <96816530+raj-ch017@users.noreply.github.com>
2023-03-28 04:33:52 +0530

    Delete first_problemo.py

--- first_problemo.py
+++
@@ -1,9 +0,0 @@
-d_km = 10
-d_mi = 1.61 * d_km
-t1 = 43
-t2 = (43*60)+30
-a1 = t2/d_mi
-a2 = d_mi/t2
-print("Distance in miles: ",d_mi)
-print("Average time per mile is: ",a1)
-print("Average speed is: ",a2)
