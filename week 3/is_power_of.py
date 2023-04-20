3e270ab6e446bdaf4c3ca5566d09e8562d36a1d0
Rajdeep Chakraborty <96816530+raj-ch017@users.noreply.github.com>
2023-04-21 04:23:04 +0530

    Create is_power_of.py

---
+++ week 3/is_power_of.py
@@ -0,0 +1,15 @@
+def is_power_of(number, base):
+  
+  if number < base and number !=1:
+   
+    return bool(0)
+
+  
+  num1 = number / base
+  if num1 == 1 or number == 1:
+    return bool(1)
+  return is_power_of(num1, base)
+
+print(is_power_of(1,2)) # Should be True
+print(is_power_of(64,4)) # Should be True
+print(is_power_of(70,10)) # Should be False
