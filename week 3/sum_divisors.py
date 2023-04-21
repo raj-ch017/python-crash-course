ef588d711e3a82b599385354dabd450b91ebe819
Rajdeep Chakraborty <96816530+raj-ch017@users.noreply.github.com>
2023-04-21 12:38:23 +0530

    Update sum_divisors.py

--- week 3/sum_divisors.py
+++ week 3/sum_divisors.py
@@ -11,15 +11,3 @@ def sum_divisors(n):
     return sum
 
 #print(sum_divisors(36))  
-
-def multiplication_table(number):
-    input1 = number
-    multiplier = 1
-    while multiplier <= 5:
-        result = input1 * multiplier
-        if result > 25:
-            break
-        print(str(number) + "x" + str(multiplier) + "=" + str(result))
-        multiplier += 1
-
-multiplication_table(2) 
